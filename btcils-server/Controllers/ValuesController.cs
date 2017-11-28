d68acd4925ef503ea5fc5150cb2e4fa8a60a2344
Maor Rosenfeld <maor.r@walkme.com>
2017-11-28 16:28:43 +0200

    Init

---
+++ btcils-server/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace btcils_server.Controllers
+{
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("api/hi")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "sup", "bro" };
+        }
+    }
+}
